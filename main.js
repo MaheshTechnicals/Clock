88350e600ca4404f48da85f9d9d22deeacba7b38
msvarma10001@gmail.com <msvarma10001@gmail.com>
2022-09-19 11:27:04 +0530

    Rdy to show time

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
+const myTime = () => {
+  const date = new Date()
+  const hours = date.getHours()
+  const minutes = date.getMinutes()
+  const seconds = date.getSeconds()
+  const am = document.getElementById("am")
+  if (hours >= 12) {
+    am.innerText = "PM"
+  }
+  else {
+    am.innerText = "AM"
+  }
 
+  if (hours > 12) {
+    hours = hours - 12
+  }
+  
+  
+  if (seconds <10) {
+    document.getElementById("sec").innerText = `0${seconds}`
+  }
+  
+  else{
+    document.getElementById("sec").innerText = seconds
+  }
+
+  document.getElementById("hou").innerText = hours
+  document.getElementById("min").innerText = minutes
+  
+
+
+
+
+
+
+}
+
+setInterval(myTime, 1000)
